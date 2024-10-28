02951717f34e7abd95bea8e30b28c696a92d4f93
jbigman <jbigman@users.noreply.github.com>
2024-10-28 12:38:00 +0100

    fix base url for images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
+let assetPrefix = ''
+let basePath = ''
+
+const isGithubActions = process.env.GITHUB_ACTIONS ?? false
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: basePath
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ["@/assets/scss/main.scss"],
