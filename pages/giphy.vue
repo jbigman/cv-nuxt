f868d2034aec9083e0ec0910e4059099a94326da
jbigman <jbigman@users.noreply.github.com>
2024-10-29 12:14:15 +0100

    Added route "What's your favorite Giphy?" story

---
+++ pages/giphy.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div class="column center" style="margin: 50px;">
+    <div class="column center" style="gap: 50px ; max-width: 800px;">
+      <h1>What's your favorite Giphy?</h1>
+      <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMG1nM2h0aG14NWY1ZmpzMzA0ZXc2Z2ZxaG5iOWNqZXBlbzExNGthdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/iD9cm5ZRKnVO0GkOdH/giphy.gif" />
+      <img src="https://media1.tenor.com/m/6Ju_FlRfSGUAAAAC/tkt-smart.gif" />
+      <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGtsNTg4bzd5MDRjamQwaWR0dWY4Y3h1aTc2cnlucnA4MWJ3ZTJ5ZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/d4zHnLjdy48Cc/giphy.gif" />
+      <img src="https://media1.tenor.com/m/esLdLk6mydMAAAAC/clapping-yay.gif" />
+      <img src="https://media1.tenor.com/m/T-6u_UYWemYAAAAC/a-few-moments-later.gif" />
+      <img src="https://media3.giphy.com/media/2gSX8o9w6WAiZlytT1/giphy.webp?cid=ecf05e47zdv0jz96200lbrub7se9fnqrt7tqqvixf6ykem98&ep=v1_gifs_search&rid=giphy.webp&ct=g" />
+      <img src="https://media1.tenor.com/m/oE7O8I0TvEwAAAAC/thats-all-folks.gif" />
+    </div>
+  </div>
+</template>
